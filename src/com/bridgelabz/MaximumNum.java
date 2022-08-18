7398ea072add979d57642c39b5c95c150dad18c6
Javul Mulla <javulmulla8@gmail.com>
2022-08-18 11:36:01 +0530

    Refactor: 3 code refactored in one generic method to find max

--- src/com/bridgelabz/MaximumNum.java
+++ src/com/bridgelabz/MaximumNum.java
@@ -3,16 +3,23 @@ package com.bridgelabz;
 public class MaximumNum {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generic Program.");
+		Integer a = 10, b = 30, c =40;
+		Float x = 11.2f, y = 11.8f, z = 11.6f;
         String j = "Cat", k = "Dog" , l = "Horse";
-	        testMaximum(j, k, l); 	}
-	 private static void testMaximum(String j,String k,String l) {
-		 String max = j;
-		if (k.compareTo(j) > 0) {
-			max = k;
+        
+        MaximumNum.getMaximum(a, b, c);
+        MaximumNum.getMaximum(x, y, z);
+        MaximumNum.getMaximum(j, k, l);
+	}
+        
+	 private static <T extends Comparable> void getMaximum(T a, T b, T c) {
+		 T max = a;
+		if (b.compareTo(a) > 0) {
+			max = b;
 		}
-		if (l.compareTo(max) > 0) {
-			max = l;
+		if (c.compareTo(max) > 0) {
+			max = c;
 		}
-		System.out.println("Maximum string is:" + max);
+		System.out.println("Maximum :" + max);
 	}
 }
