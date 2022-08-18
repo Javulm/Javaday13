6183c81a7208939bbe3e93be96833f0009c52e8c
Javul Mulla <javulmulla8@gmail.com>
2022-08-18 07:49:14 +0530

    Modified: Creted generic class to take 3 generic type variables

--- src/com/bridgelabz/MaximumNum.java
+++ src/com/bridgelabz/MaximumNum.java
@@ -1,22 +1,26 @@
 package com.bridgelabz;
 
-public class MaximumNum {
+public class MaximumNum <T extends Comparable> {
+	   T var1, var2, var3;
+	    public FindMaximum(T var1, T var2, T var3) {
+	        this.var1 = var1;
+	        this.var2 = var2;
+	        this.var3 = var3;
+	    }
 	public static void main(String[] args) {
-		// 1st print welcome msg.
 		System.out.println("Welcome to Generic Program.");
-		// variables integer , float , and string
 		Integer a = 20, b = 50, c = 80;
 		Float p = 20.85f, q = 90.55f, r = 80.98f;
 		String e = "Apple", f = "Peach", g = "Banana";
-		/* calling generic getMethod directly coz in main method and generics
-		method using static keyword so not needed to create object for class.*/
-		FindMaximum.getMaximum(a, b, c);
-		FindMaximum.getMaximum(p, q, r);
-		FindMaximum.getMaximum(e, f, g);
+		new FindMaximum.getMaximum(a, b, c).max();
+		new FindMaximum.getMaximum(p, q, r).max();
+		new FindMaximum.getMaximum(e, f, g).max();
 	}
-	// create generic method the Generic Type extends Comparable
+	 private void max() {
+	        FindMaximum.getMaximum(var1, var2, var3);
+
+	    }
 	private static <T extends Comparable> void getMaximum(T a, T b, T c) {
-		// check the condition and finding max for integer string and float
 		T max = a;
 		if (b.compareTo(a) > 0) {
 			max = b;
@@ -24,7 +28,6 @@ public class MaximumNum {
 		if (c.compareTo(max) > 0) {
 			max = c;
 		}
-		// print maximum no and string
 		System.out.println("Maximum ::" + max);
 	}
 }
