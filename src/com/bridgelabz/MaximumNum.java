82b9876d591a2ba57f121179a157cd395abf15c5
Javul Mulla <javulmulla8@gmail.com>
2022-08-18 13:35:08 +0530

    Modfied: Extended maximum method to take more than three parameters

--- src/com/bridgelabz/MaximumNum.java
+++ src/com/bridgelabz/MaximumNum.java
@@ -1,35 +1,21 @@
 package com.bridgelabz;
 
-public class MaximumNum <T extends Comparable>{
-	 T var1, var2, var3;
-	 public MaximumNum(T var1, T var2, T var3) {
-	        this.var1 = var1;
-	        this.var2 = var2;
-	        this.var3 = var3;
+public class MaximumNum <T>{
+	 public static <T extends  Comparable <T>>  T maximumValue( T val1 , T val2, T val3 ,T val4,T val5) {
+	        T max = val1;
+	        if (val2.compareTo(max) > 0 ) {
+	            max = val2;
+	        } if (val3.compareTo(max) > 0){
+	            max = val3;
+	        }if (val4.compareTo(max) > 0 ){
+	            max = val4;
+	        }if (val5.compareTo(max) > 0){
+	            max = val5;
+	        }
+	        return max;
 	    }
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generic Program.");
-		Integer a = 10, b = 30, c =40;
-		Float x = 11.2f, y = 11.8f, z = 11.6f;
-        String j = "Cat", k = "Dog" , l = "Horse";
-        
-     new MaximumNum<>(a, b, c).max();
-     new MaximumNum<>(x, y, z).max();
-     new MaximumNum<>(j, k, l).max();
-	}
-        
-	 private void max() {
-		// TODO Auto-generated method stub
-		MaximumNum.getMaximum(var1, var2, var3);
-	}
-	private static <T extends Comparable> void getMaximum(T a, T b, T c) {
-		 T max = a;
-		if (b.compareTo(a) > 0) {
-			max = b;
-		}
-		if (c.compareTo(max) > 0) {
-			max = c;
-		}
-		System.out.println("Maximum :" + max);
+	    System.out.println( "The maximum value is :: "+maximumValue(3,3,6,7,1));
 	}
 }
